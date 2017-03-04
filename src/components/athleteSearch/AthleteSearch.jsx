b39a8ca0873bbddcc421b6b2ec050685ca75ad17
Blair Garrett <blair.garrett@sky.uk>
2017-03-04 19:20:48 +0000

    Made it flow compatiable and passes linting

--- src/components/athleteSearch/AthleteSearch.jsx
+++ src/components/athleteSearch/AthleteSearch.jsx
@@ -2,34 +2,54 @@
 
 import React from 'react';
 
+import type {InputEvent} from '../../types/InputEvent.js';
+
 import './AthleteSearch.scss';
 
+type Props = {};
+
 export default class AthleteSearch extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {athleteId: ''};
-      this.handleChange = this.handleChange.bind(this);
-      this.handleSubmit = this.handleSubmit.bind(this);
-    }
+  props: Props;
 
-    handleChange(event) {
-      this.setState({athleteId: event.target.value});
-    }
+  state: {
+      athleteId: string;
+  };
 
-    handleSubmit(e) {
-      e.preventDefault();
-      //@todo: Fire off to a back end event.
-    }
+  handleChange: () => void;
+  handleSubmit: () => void;
 
-    render() {
-        return <div>
-                <form onSubmit={this.handleSubmit}>
-                  <label htmlFor="athleteSearchBox">Search For Athlete</label>
-                  <input type="text" key="athleteSearchBox" value={this.state.athleteId}
-                         onChange={this.handleChange} className="athleteSearchBox" />
-                  <br />
-                  <button type="submit">Search</button>
-                </form>
-               </div>;
-    }
+  constructor(props: Props) {
+    super(props);
+    this.state = {athleteId: ''};
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange = (event: InputEvent) => {
+    this.setState({athleteId: event.target.value});
+  };
+
+  handleSubmit = (event: InputEvent) => {
+    event.preventDefault();
+    //@todo: Fire off to a back end event.
+    console.log(this.state.athleteId);
+  };
+
+  render = () => {
+    return <div>
+      <form onSubmit={this.handleSubmit}>
+        <label htmlFor="athleteSearchBox">
+          Search For Athlete
+        </label>
+        <input
+          type="text"
+          key="athleteSearchBox"
+          value={this.state.athleteId}
+          onChange={this.handleChange}
+          className="athleteSearchBox" />
+        <br />
+        <button type="submit">Search</button>
+      </form>
+    </div>;
+  };
 }
