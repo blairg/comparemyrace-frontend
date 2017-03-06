7e3d876cce2b892aa4b4a73f713dcc8969075006
Blair Garrett <blair.garrett@sky.uk>
2017-03-06 20:19:42 +0000

    Added tests using Enzyme

--- api/src/server.js
+++ api/src/server.js
@@ -3,8 +3,8 @@ const koa = require('koa');
 const app = koa();
 
 app.use(function* () {
-  this.body = 'Hello from koajs';
   console.log('API started!!');
+  yield this.body = 'Hello from koajs';
 });
 
 app.listen(3000);
--- tests/test_helper.js
+++ tests/test_helper.js
@@ -1,4 +1,6 @@
 /* eslint-disable */
+require('babel-register')();
+
 import jsdom from 'jsdom';
 import chai from 'chai';
 import chaiImmutable from 'chai-immutable';
@@ -16,3 +18,26 @@ Object.keys(window).forEach((key) => {
 });
 
 chai.use(chaiImmutable);
+
+
+
+/*require('babel-register')();
+
+var jsdom = require('jsdom').jsdom;
+
+var exposedProperties = ['window', 'navigator', 'document'];
+
+global.document = jsdom('');
+global.window = document.defaultView;
+Object.keys(document.defaultView).forEach((property) => {
+  if (typeof global[property] === 'undefined') {
+    exposedProperties.push(property);
+    global[property] = document.defaultView[property];
+  }
+});
+
+global.navigator = {
+  userAgent: 'node.js'
+};
+
+documentRef = document;*/
