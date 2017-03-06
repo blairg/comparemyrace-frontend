f681dae8743f9d97c969f5fcaeb69e84b977489e
Blair Garrett <blair.garrett@sky.uk>
2017-03-06 18:11:59 +0000

    Added Yarn support

--- api/src/server.js
+++ api/src/server.js
@@ -2,8 +2,9 @@ const koa = require('koa');
 
 const app = koa();
 
-app.use(() => {
+app.use(function* () {
   this.body = 'Hello from koajs';
+  console.log('API started!!');
 });
 
 app.listen(3000);
