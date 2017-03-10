4351fad05089efb8ac1214df8f7d24031a594d32
Blair Garrett <blair.garrett@sky.uk>
2017-03-10 21:34:25 +0000

    fixed eslint issues with koa generator

--- api/src/server.js
+++ api/src/server.js
@@ -1,10 +1,16 @@
-const koa = require('koa');
+const Koa = require('koa');
+const router = require('koa-router')();
 
-const app = koa();
+const app = new Koa();
 
-app.use(function* () {
-  console.log('API started!!');
-  yield this.body = 'Hello from koajs';
+router.get('/', function* base(next) {
+  this.status = 200;
+  this.body = 'Hello from Koa!';
+
+  yield next;
 });
+
+app.use(router.routes());
+app.use(router.allowedMethods());
 
 app.listen(3000);
