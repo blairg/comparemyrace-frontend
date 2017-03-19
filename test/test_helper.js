8fde7a173b51debf533dbffd4272e6b19456c0bc
Blair Garrett <blair.garrett@sky.uk>
2017-03-19 22:31:34 +0000

    adding tests

---
+++ test/test_helper.js
@@ -0,0 +1,43 @@
+/* eslint-disable */
+require('babel-register')();
+
+import jsdom from 'jsdom';
+import chai from 'chai';
+import chaiImmutable from 'chai-immutable';
+
+const doc = jsdom.jsdom('<!doctype html><html><body></body></html>');
+const win = doc.defaultView;
+
+global.document = doc;
+global.window = win;
+
+Object.keys(window).forEach((key) => {
+    if (!(key in global)) {
+        global[key] = window[key];
+    }
+});
+
+chai.use(chaiImmutable);
+
+
+
+/*require('babel-register')();
+
+var jsdom = require('jsdom').jsdom;
+
+var exposedProperties = ['window', 'navigator', 'document'];
+
+global.document = jsdom('');
+global.window = document.defaultView;
+Object.keys(document.defaultView).forEach((property) => {
+  if (typeof global[property] === 'undefined') {
+    exposedProperties.push(property);
+    global[property] = document.defaultView[property];
+  }
+});
+
+global.navigator = {
+  userAgent: 'node.js'
+};
+
+documentRef = document;*/
