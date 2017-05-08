a90f12344fb023b64992940dfe950db54af2b1ac
Blair Garrett <blair.garrett@sky.uk>
2017-05-08 08:24:19 +0100

    Docker support all done

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 export default class Config {
   static dev = {
     accessCodeUrl: 'https://www.strava.com/oauth/authorize?client_id=15787&response_type=code&redirect_uri=http://localhost:8080/token_exchange&scope=view_private&state=&approval_prompt=auto',
-    accessTokenUrl: 'http://comparemyrace-frontend-client:3000/token/',
+    accessTokenUrl: 'http://localhost:3000/token/',
   };
 
   static stage = {};
